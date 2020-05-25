6caa63b57bd0cb89aa763be91c38574c770bd2aa
Florin Saftoiu <florin.saftoiu@soft-to-you.com>
2020-05-26 01:55:49 +0300

    use logits for loss fn, more numerically stable

--- tf.py
+++ tf.py
@@ -15,11 +15,13 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(input_shape=(28, 28)),
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(10, activation='softmax')
+    tf.keras.layers.Dense(10)
 ])
 
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
 model.compile(optimizer='adam',
-        loss='sparse_categorical_crossentropy',
+        loss=loss_fn,
         metrics=['accuracy'])
 
 model.fit(x_train, y_train, epochs=5)
