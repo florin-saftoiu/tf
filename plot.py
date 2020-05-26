e221354454004e71549c01a726a2c9faa53cfb43
Florin Saftoiu <florin.saftoiu@soft-to-you.com>
2020-05-26 14:37:15 +0300

    plot some mnist images

--- plot.py
+++ plot.py
@@ -1,4 +1,18 @@
+import tensorflow as tf
 import matplotlib.pyplot as plt
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5, 5, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(x_train[i], cmap=plt.cm.binary)
+    plt.xlabel(y_train[i])
+plt.show()
 
 fig, ax = plt.subplots()
 ax.plot([1, 2, 3, 4], [1, 4, 2, 3])
